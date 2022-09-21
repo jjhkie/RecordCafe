858b2d9229f30e96084f3907d7a1b92b5735396f
jjhkie <jjhkie0916@naver.comddd>
2022-09-21 17:35:13 +0900

    [ api 정보 가져오기 ] api 정보 가져오기

--- AroundCafe/Presentation/LocationInfo/LocationInformationViewModel.swift
+++ AroundCafe/Presentation/LocationInfo/LocationInformationViewModel.swift
@@ -26,15 +26,48 @@ struct LocationInformationViewModel{
     let currentLocationButtonTapped = PublishRelay<Void>()
     let detailListItemSelected = PublishRelay<Int>()
     
-    let documentData = PublishSubject<[KLDocument?]>()
+    private let documentData = PublishSubject<[KLDocument]>()
     
-    init(){
+    init(model: LocationInformationModel = LocationInformationModel()){
+        
+        //네트워크 통신으로 데이터 불러오기
+        let cvsLocationDataResult = mapCenterPoint
+            .flatMapLatest(model.getLocation)
+            .share()
+        
+        let cvsLocationDataValue = cvsLocationDataResult
+            .compactMap{data -> LocationData? in
+                guard case let .success(value) = data else {
+                    return nil
+                }
+                return value
+            }
+        
+        let cvsLocationDataErrorMessage = cvsLocationDataResult
+            .compactMap{ data -> String? in
+                switch data{
+                case let .success(data) where data.documents.isEmpty:
+                    return """
+                        500m 근처에 이용할 수 없는 카페가 없습니다.
+                        재검색해주세요
+                        """
+                case let .failure(error):
+                    return error.localizedDescription
+                default:
+                    return nil
+                }
+            }
+        
+        cvsLocationDataValue
+            .map{ $0.documents}
+            .bind(to: documentData)
+            .disposed(by: disposeBag)
+        
         //지도 중심점 설정
         let selectDetailListItem = detailListItemSelected
             .withLatestFrom(documentData){ $1[$0]}
             .map{data -> MTMapPoint in
-                guard let data = data,
-                      let longtitue = Double(data.x),
+                guard let longtitue = Double(data.x),
                       let latitude = Double(data.y) else { return MTMapPoint()}
                 let geoCoord = MTMapPointGeo(latitude: latitude, longitude: longtitue)
                 return MTMapPoint(geoCoord: geoCoord)
@@ -53,10 +86,21 @@ struct LocationInformationViewModel{
             .asSignal(onErrorSignalWith: .empty())
         
         //에러 메세지 전달
-        errorMessage = mapViewError.asObservable()
+        errorMessage = Observable
+            .merge(
+                cvsLocationDataErrorMessage,
+                mapViewError.asObservable()
+            )
             .asSignal(onErrorJustReturn: "잠시 후 다시 시도해주세요")
         
-        detailListCellData = Driver.just([])
+        detailListCellData = documentData
+            .map(model.documentsToCellData)
+            .asDriver(onErrorDriveWith: .empty())
+        
+        documentData
+            .map{!$0.isEmpty}
+            .bind(to: detailListBackgroundViewModel.shouldHideStatusLabel)
+            .disposed(by: disposeBag)
         
         scrollToSelectedLocation = selectPOIItem
             .map{$0.tag}
