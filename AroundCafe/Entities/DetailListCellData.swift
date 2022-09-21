eeafeb3366f7c9a4133d8d218cf90905f2d218bd
jjhkie <jjhkie0916@naver.comddd>
2022-09-21 09:48:37 +0900

    [ 초기 설정 ] API 연결 설정

---
+++ AroundCafe/Entities/DetailListCellData.swift
@@ -0,0 +1,10 @@
+
+
+import Foundation
+
+struct DetailListCellData {
+    let placeName: String
+    let address: String
+    let distance: String
+    let point: MTMapPoint
+}
---
+++ AroundCafe/Presentation/LocationInformationViewController.swift
@@ -0,0 +1,7 @@
+
+
+import UIKit
+
+class LocationInformationViewController: UIViewController{
+    
+}
